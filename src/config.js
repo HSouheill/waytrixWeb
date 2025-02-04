bdd9750b61910d005b8936d37d6281e9f2f96220
HSouheill <husseinsouheil15@gmail.com>
2025-02-04 11:32:07 +0200

    sec commit

--- src/config.js
+++ src/config.js
@@ -3,4 +3,5 @@
 
 
 // export const ipAddress = 'http://192.168.0.114:4000'; // Change this to your actual IP address
-export const ipAddress = 'https://waytrix-backend-2qz4.onrender.com'; // Change this to your actual IP address
+// export const ipAddress = 'https://waytrix-backend-2qz4.onrender.com'; // Change this to your actual IP address
+export const ipAddress = 'https://oldwaytrixbackend.onrender.com';
